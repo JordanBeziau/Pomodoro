be53ecd1353babd611a92c19d1f378c00abfb3cb
Jordan Beziau <jordanbeziau@mbp-de-jordan.home>
2017-05-21 12:59:01 +0200

    add CSS styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  let time = 1500,
+  let time = 1499,
     count = 0,
     seconds, minutes;
 
   const controlButton = document.getElementById('control-button'),
-    timeContainer = document.querySelector('time');
-  let controlButtonCheck = false;
+    timeContainer = document.querySelector('time'),
+    title = document.querySelector('title'),
+    button = document.querySelector('.control');
+  let controlButtonCheck = true;
+  let pomodoro;
 
   const startPomodoro = () => {
     if (time == 0) {
@@ -35,14 +38,14 @@ document.addEventListener('DOMContentLoaded', () => {
     minutes = Math.floor(time % 60);
     seconds <= 9 ? seconds = `0${seconds}` : '';
     minutes <= 9 ? minutes = `0${minutes}` : '';
+    title.innerHTML = `Pomodoro - ${minutes} : ${seconds}`;
     timeContainer.innerHTML = `${minutes} : ${seconds}`;
   }
 
   controlButton.addEventListener('mouseup', () => {
     controlButtonCheck ? pomodoro = setInterval(startPomodoro, 1000) : clearInterval(pomodoro);
+    controlButtonCheck ? button.className = 'control pause' : button.className = 'control play';
     controlButtonCheck = !controlButtonCheck;
   });
 
-  startPomodoro();
-  let pomodoro = setInterval(startPomodoro, 1000);
 });
